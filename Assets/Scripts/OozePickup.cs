5d6b885260dc1c9a676a90135214594ba73bd58e
IamJappe <deblock.jasper2007@gmail.com>
2023-01-29 16:40:19 +0100

    Player animation fixed + ooze pickup

---
+++ Assets/Scripts/OozePickup.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OozePickup : MonoBehaviour
+{
+
+    public OozeCounter oozeScript;
+
+    [SerializeField] int dropAmount;
+    [SerializeField] float size;
+
+    private void Start()
+    {
+        dropAmount = Random.Range(1, 10);
+
+        if (dropAmount <= 3)
+        {
+            size = 1;
+        }
+        if(dropAmount >= 4 && dropAmount <= 7)
+        {
+            size = 2;
+        }
+        if (dropAmount >= 8 && dropAmount <= 10)
+        {
+            size = 3;
+        }
+        transform.localScale = new Vector3(size/2,size/2,size/2);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            oozeScript.AddAmount(dropAmount);
+            Debug.Log(dropAmount + " ooze picked up.");
+            Destroy(gameObject);
+        }
+    }
+}
