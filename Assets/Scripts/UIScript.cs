4e812874174763a0fcdda4cc1f879e6772fc3eee
IamJappe <deblock.jasper2007@gmail.com>
2023-01-30 20:40:15 +0100

    options menu

--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -9,15 +9,34 @@ public class UIScript : MonoBehaviour
     public GameObject menuButtons;
     public OozeCounter oozeCounter;
     public BlendingCams blendScript;
+    public GameObject buttonPanel;
+    public GameObject optionsPanel; 
 
     void Update()
     {
         oozeText.text = "Ooze: " + oozeCounter.amount.ToString() + "/" + oozeCounter.maxAmount.ToString();
+
+        if (Input.GetKeyDown(KeyCode.Escape) && optionsPanel.active == true)
+        {
+            buttonPanel.SetActive(true);
+            optionsPanel.SetActive(false);
+        }
     }
 
     public void Play()
     {
         blendScript.blendCams(1, 2);
         menuButtons.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Options()
+    {
+        buttonPanel.SetActive(false);
+        optionsPanel.SetActive(true);
     }
 }
